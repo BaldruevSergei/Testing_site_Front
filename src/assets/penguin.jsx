ff7676ed3ed80886d696d493d0b4e9fc0ba60e2e
Erkhembileg0803 <b.erkhembileg0803@gmail.com>
2025-01-30 20:58:09 +0800

    Main page

---
+++ src/assets/penguin.jsx
@@ -0,0 +1,4 @@
+function Penguin(){
+     return 
+}
+export default Penguin;
---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import '../styles/footer.css'
+function Footer() {
+     return <footer>
+               <hr />
+               <ul className='footerInfo'>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem, ipsum dolor.</li>
+               </ul>
+          </footer>
+}
+export default Footer;
