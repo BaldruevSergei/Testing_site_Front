c15bc5c885b266b9fcb2280a0e769469777b5d96
Erkhembileg0803 <b.erkhembileg0803@gmail.com>
2025-02-05 08:37:28 +0800

    Loginpage smooth

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,26 +19,28 @@ function Header() {
 }
 function LoginMenu() {
      const [menuOpen, setMenuOpen] = useState(false);
+
      const toggleMenu = () => {
           setMenuOpen(!menuOpen);
      };
+
      return (
           <div>
                <div className="accountDashboard">
                     <button type="button" onClick={toggleMenu}>Зайти</button>
                     <div className="userProfile"></div>
-               </div>
-               {menuOpen && (
-                    <div className="login-menu">
-                         <ul>
-                              <li><Link to='/LoginPage/teacher' onClick={() => setMenuOpen(false)}>Учитель</Link></li>
-                              <li><Link to='/LoginPage/student' onClick={() => setMenuOpen(false)}>Студент</Link></li>
-                              <li><Link to='/LoginPage/admin' onClick={() => setMenuOpen(false)}>Админ</Link></li>
-                         </ul>
+                    <div className="login-menu" style={{ height: menuOpen ? '200px' : '0', border: menuOpen ? '' : 'transparent'}}>
+                         {menuOpen && (
+                              <ul>
+                                   <li><Link to='/LoginPage/teacher' onClick={() => setMenuOpen(false)}>Учитель</Link></li>
+                                   <li><Link to='/LoginPage/student' onClick={() => setMenuOpen(false)}>Студент</Link></li>
+                                   <li><Link to='/LoginPage/admin' onClick={() => setMenuOpen(false)}>Админ</Link></li>
+                              </ul>
+                         )}
                     </div>
-               )}
+               </div>
           </div>
-     )
+     );
 }
 function ResponsiveMenu() {
      const [menuOpen, setMenuOpen] = useState(false);
