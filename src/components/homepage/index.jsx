9f9c1ce09dba54686f54559a656047578e162108
Erkhembileg <b.erkhembileg0803@gmail.com>
2025-03-10 12:11:46 +0800

    homepage intro

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,6 +1,7 @@
 import './homepage.scss';
 import Header from '../header';
 import Footer from '../footer';
+import { Link } from 'react-router-dom';
 export default function Homepage(){
      return <div className='homepage' >
           <Header/>
@@ -13,7 +14,15 @@ function Main(){
      return <>
      <main>
           <nav>
-
+               <div>
+                    <div>👋Легкий доступ для учителей.</div>
+               <h1>Платформа для тестов и обучения</h1>
+               <p>Онлайн-платформа для школьников, позволяющая проходить тесты, улучшать знания, проверять результаты и готовиться к экзаменам легко и удобно.</p>
+                    <form onSubmit={(e) => e.preventDefault()}>
+                    <Link>Начало</Link>
+                    <Link>Рабочая панель</Link>
+                    </form>
+               </div>
           </nav>
      </main>
      </>
