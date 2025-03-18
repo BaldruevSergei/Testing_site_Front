47bf1059e88f5033471592720e6f03a74d1d72ae
erkhembileg <b.erkhembileg0803@gmail.com>
2025-03-18 15:51:12 +0800

    better test run responsive

--- src/components/testrunpage/testrun.jsx
+++ src/components/testrunpage/testrun.jsx
@@ -3,6 +3,9 @@ import './testrun.scss';
 import { useParams , Link} from 'react-router-dom';
 import SideBar from './sidebar';
 import { useEffect, useState } from 'react';
+import MultipleChoice from './typesofquestion/multiple';
+import SingleChoice from './typesofquestion/single';
+import Input from './typesofquestion/input';
 export default function TestRun(){
     const {id} = useParams();
     const [test, setTest] = useState({});
@@ -10,38 +13,40 @@ export default function TestRun(){
     useEffect(() => {
         setTest(Tests.find((test) => test.id === parseInt(id)));
     },[id])
-    return <>
+    return (<>
+        {test.questions && test.questions.length > 0 && (
         <div className="testrun">
-            <main>
-                <section>
-                <div className='topic'>Рабочая панель <span>/</span> <Link to={`/TestLibrary/TestPreview/${id}`}>{test.topic}</Link></div>
-                {test.questions && test.questions.length > 0 && (
-                    <h1>{currentQuestion + 1}. {test.questions[currentQuestion].title.charAt(0).toUpperCase() + test.questions[currentQuestion].title.slice(1)}</h1>
-                )}
-                <div className="container">
-                {test.questions && test.questions.length > 0 && (
-                    <h1>Условие задания: <div>{test.questions[currentQuestion].points}б</div></h1>
-                )}  
-                <div className='questionContainer'>
-                    <div className='question'>
-                        {test.questions && test.questions.length > 0 && (
-                        test.questions[currentQuestion].question
-                        )}  
-                    </div>
-                    <div className='answer'>
-                        {test.questions && test.questions.length > 0 && test.questions[currentQuestion].type === 'input' && (
-                            <>
-                                <div>Ответ: <input type="text" /></div>
-                                <button>Ответить</button>
-                            </>
-                        )}
-                    </div>
+        <main>
+            <section>
+            <div className='topic'>Рабочая панель <span>/</span> <Link to={`/TestLibrary/TestPreview/${id}`}>{test.topic}</Link></div>
+                <h1>{currentQuestion + 1}. {test.questions[currentQuestion].title.charAt(0).toUpperCase() + test.questions[currentQuestion].title.slice(1)}</h1>
+            <div className="container" >
+                <h1>Условие задания: <div>{test.questions[currentQuestion].points}б</div></h1>
+            <div className='questionContainer'>
+                <div className='question'  onMouseDown={(e) => e.preventDefault()}>
+                    {test.questions[currentQuestion].question}
                 </div>
+                <div className='answer'>
+                    {test.questions[currentQuestion].type === 'input' && (
+                        <Input answers={test.questions[currentQuestion].answers} currentQuestion={currentQuestion}/>
+                    )}
+                    {test.questions[currentQuestion].type === 'singleChoice' && (
+                        <SingleChoice answers={test.questions[currentQuestion].answers} currentQuestion={currentQuestion}/>
+                    )}
+                    {test.questions[currentQuestion].type === 'multipleChoice' && (
+                        <MultipleChoice answers={test.questions[currentQuestion].answers} currentQuestion={currentQuestion}/>
+                    )}
                 </div>
-                <div className="arrows"></div>
-                </section>
-            </main>
-            <SideBar id={id} numQuestions={test.numQuestions} maxTime={test.maxTime} currentQuestion={currentQuestion} setQuestion={setQuestion}/>
-        </div>
-    </>
+            </div>
+            </div>
+            <div className="arrows">
+                    <span style={{display: currentQuestion > 0 ? 'flex' : 'none'}} onClick={() => {currentQuestion > 0 ? setQuestion(prev => prev - 1) : ''}}><i className="fa fa-arrow-circle-left"></i>Предыдущее задание</span>
+                    <span style={{display: test.questions.length - 1 > currentQuestion ? 'flex' : 'none'}} onClick={() => {test.questions.length > currentQuestion ? setQuestion(prev => prev + 1) : ''}}><i className="fa fa-arrow-circle-right"></i>Следующее задание</span>
+            </div>
+            </section>
+        </main>
+        <SideBar id={id} numQuestions={test.numQuestions} maxTime={test.maxTime} currentQuestion={currentQuestion} setQuestion={setQuestion}/>
+    </div>
+        )}
+    </>)
 }
