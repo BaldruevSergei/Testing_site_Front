9c0a8ede9bcc593a99f5ce1018dfaabc53796708
erkhembileg <b.erkhembileg0803@gmail.com>
2025-05-13 14:55:18 +0800

    random

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
-import { setupLoginMock } from './mock/loginmock.jsx';
-
-if (import.meta.env.MODE === 'development') {
-  setupLoginMock();
-}
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
